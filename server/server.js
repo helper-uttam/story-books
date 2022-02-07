55d8bc49e1a21b92f69086b4c51db5dbd83f0a5a
unknown <bajiraouttamsinha@gmail.com>
2022-02-07 20:12:59 +0530

    deployement

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-const containerURL = "mongodb://mongo:27017/assignment";
 const url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.axnkt.mongodb.net/assignment?retryWrites=true&w=majority`;
 mongoose.connect(url, {useNewUrlParser: true});
 const connection = mongoose.connection;
