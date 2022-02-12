f87f8c43c3b3ecae9dc22408b7b419ba54c9ad13
unknown <bajiraouttamsinha@gmail.com>
2022-02-12 15:47:50 +0530

    using docker-images

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -46,55 +45,3 @@ app.post('/:to', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 })
-=======
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
-
-require('dotenv').config();
-
-const app = express();
-const port = process.env.PORT || 5000;
-
-
-const msg = (userNumber, bodyOfMsg) => {
-    const accountSid = process.env.TWILIO_ACCOUNT_SID;
-    const authToken = process.env.TWILIO_AUTH_TOKEN;
-    const client = require('twilio')(accountSid, authToken);
-    client.messages 
-      .create({ 
-         body: bodyOfMsg, 
-         from: `whatsapp:+${process.env.FROM}`,       
-         to: `whatsapp:+${userNumber}` 
-       }) 
-      .then(message => console.log(message)) 
-      .done();
-
-}
-
-app.use(cors());
-app.use(express.json());
-const url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.axnkt.mongodb.net/assignment?retryWrites=true&w=majority`;
-mongoose.connect(url, {useNewUrlParser: true});
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('MongoDB is conected');
-});
-
-const storyRouter = require('./routes/stories');
-const userRouter = require('./routes/users');
-
-app.use('/stories', storyRouter);
-app.use('/users', userRouter);
-
-//twilio endpoint
-app.post('/:to', (req, res) => {
-    msg(req.params.to, req.body.story);
-    res.send('msg sent')
-})
-
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-})
->>>>>>> 91d71163bf312c5873a2733d9bb0888e370362d4
